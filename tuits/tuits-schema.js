52586e69cd891630cd25e7c6586141358c18c606
Zhe Xi <zhexi@ZhedeMacBook-Pro.local>
2023-04-01 21:40:48 -0700

    a9 Mongo fa22

---
+++ tuits/tuits-schema.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema({
+  topic: String,
+  userName: String,
+  handle: String,
+  time: String,
+  image: String,
+  title: String,
+  tuit: String,
+  likes: Number,
+  liked: Boolean,
+  dislikes: Number,
+  disliked: Boolean,
+  replies: String,
+  retuits: String,
+}, {collection: 'tuits'});
+export default schema;
